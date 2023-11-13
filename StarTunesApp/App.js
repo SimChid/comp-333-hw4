3f886d48493586dc52befa21c044444a7dfed3ad
SimChid <schidley@wesleyan.edu>
2023-11-13 09:27:21 -0500

    songlist view (issue 7)

--- StarTunesApp/App.js
+++ StarTunesApp/App.js
@@ -2,14 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import SignInUp from './SignInUp';
+import SongList from './SongList';
 
 export default function App() {
-  let [loggedIn,setLoggedIn] = React.useState(false);
+  let [loggedIn,setLoggedIn] = React.useState(true);
 
   if (loggedIn) {
     return(
       <View style = {styles.container}>
         <Text>Successfully logged in! </Text>
+        <SongList />
       </View>
     );
   }else{
